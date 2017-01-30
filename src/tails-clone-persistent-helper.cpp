b2144169de23cd12a59a84f6b77a22ba9dd920b6
Andrew Gallagher <andrewg@andrewg.com>
2017-01-30 02:46:53 +0000

    Simplify the setuid wrapper

--- src/tails-clone-persistent-helper.cpp
+++ src/tails-clone-persistent-helper.cpp
@@ -4,17 +4,16 @@
 #include <cstring>
 #include <unistd.h>
 
-int main(int ARGC, char **ARGV) {	
-	// Transpose our arguments, if they exist
-	std::string arg1, arg2, arg3;
-	if(ARGC >1) arg1 = ARGV[1];
-	if(ARGC >2) arg1 = ARGV[2];
-	if(ARGC >3) arg1 = ARGV[3];
-	
+int main(int ARGC, char **ARGV) {		
 	// Bake in our setuid
 	setreuid(0,0);
 	
 	// Now call our perl script and let it handle any syntax errors
-	system((std::string("/usr/bin/tails-clone-persistent-helper.pl ")
-			+ arg1 + " " + arg2 + " " + arg3).c_str());
+	if(ARGC==4) {
+		system((std::string("/usr/bin/tails-clone-persistent-helper.pl ")
+			+ ARGV[1] + " " + ARGV[2] + " " + ARGV[3]).c_str());
+	} else {
+		// call with no arguments to get the usage summary
+		system((std::string("/usr/bin/tails-clone-persistent-helper.pl").c_str())
+	}
 }
